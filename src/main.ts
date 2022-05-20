5800add9aa7588f110c889fe38ccb21f1d6d00d4
Atteroot <yuora556@gmail.com>
2022-05-20 16:23:26 +0300

    port added to dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  let port = parseInt(process.env.PORT, 10);
+
+  if (port == null) {
+    port = 8000;
+  }
+
   app.enableCors();
-  await app.listen(8000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
